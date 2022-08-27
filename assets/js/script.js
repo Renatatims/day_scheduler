3ef10b3a26e656a77f5752f69007347293f40aa8
ReTims <renatatims@gmail.com>
2022-08-27 13:54:04 -0500

    Changed table to div containers + adjusted script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var currentHour = moment().format("HH");
 function colorCode() {
     $('.hour').each(function () {
         var toNumber = parseInt($(this).data('time'));
-        var textarea = $(this).siblings("td.textarea");
+        var textarea = $(this).siblings("input.textarea");
         //Color Red if the row corresponds to the current hour
         if (toNumber == currentHour) {
             textarea.css('background-color', "red")
@@ -42,24 +42,23 @@ var floppySave = $(".saveBtn");
 
 floppySave.on("click", function (event) {
     event.preventDefault();
-    var hour = $(this).siblings("th.hour").data("time");
-    var input = $(this).siblings("td.textarea").children().val();
+    var hour = $(this).siblings("div.hour").data("time");
+    var input = $(this).siblings("input.textarea").val();
     localStorage.setItem(hour, input);
 });
 
 // Function Init - Refresh the page - events persists - get's the info from the local storage//
 
 function init() {
-    $("th[data-time='09']").siblings("td.textarea").children().val(localStorage.getItem("09"));
-    $("th[data-time='10']").siblings("td.textarea").children().val(localStorage.getItem("10"));
-    $("th[data-time='11']").siblings("td.textarea").children().val(localStorage.getItem("11"));
-    $("th[data-time='12']").siblings("td.textarea").children().val(localStorage.getItem("12"));
-    $("th[data-time='13']").siblings("td.textarea").children().val(localStorage.getItem("13"));
-    $("th[data-time='14']").siblings("td.textarea").children().val(localStorage.getItem("14"));
-    $("th[data-time='15']").siblings("td.textarea").children().val(localStorage.getItem("15"));
-    $("th[data-time='16']").siblings("td.textarea").children().val(localStorage.getItem("16"));
-    $("th[data-time='17']").siblings("td.textarea").children().val(localStorage.getItem("17"));
+    $("div[data-time='09']").siblings("input.textarea").val(localStorage.getItem("09"));
+    $("div[data-time='10']").siblings("input.textarea").val(localStorage.getItem("10"));
+    $("div[data-time='11']").siblings("input.textarea").val(localStorage.getItem("11"));
+    $("div[data-time='12']").siblings("input.textarea").val(localStorage.getItem("12"));
+    $("div[data-time='13']").siblings("input.textarea").val(localStorage.getItem("13"));
+    $("div[data-time='14']").siblings("input.textarea").val(localStorage.getItem("14"));
+    $("div[data-time='15']").siblings("input.textarea").val(localStorage.getItem("15"));
+    $("div[data-time='16']").siblings("input.textarea").val(localStorage.getItem("16"));
+    $("div[data-time='17']").siblings("input.textarea").val(localStorage.getItem("17"));
 };
-
 
 init();
